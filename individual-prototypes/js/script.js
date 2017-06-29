ca5169bd50b0a46d3f6783688139071ed8c4363c
Michael James <thelivingpc@gmail.com>
2017-06-29 14:17:24 -0700

    buttons with plus icons

--- individual-prototypes/js/script.js
+++ individual-prototypes/js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
 
-$(".add-fb").click(function() {
-	console.log("hi!");
-	var name = $(this).text();
-	$("#fb-written").append('<div class="form-group"><label for="name">' + name + '</label><textarea class="form-control" rows="3" id="' + name + '"></textarea></div>');
-	// var panel = $(this).closest(".panel");
-	// var title = panel.find(".panel-title").text();
-	// var link = "";
-	// var contents = '<div class="alert alert-success" role="alert"><strong>Well done!</strong> You successfully commented on ' + title + '. ' + link + '</div>';
-	// panel.replaceWithPush(contents).delay(1000).fadeOut();
-});
+// $(".add-fb").click(function() {
+// 	console.log("hi!");
+// 	var name = $(this).text();
+// 	$("#fb-written").append('<div class="form-group"><label for="name">' + name + '</label><textarea class="form-control" rows="3" id="' + name + '"></textarea></div>');
+// 	// var panel = $(this).closest(".panel");
+// 	// var title = panel.find(".panel-title").text();
+// 	// var link = "";
+// 	// var contents = '<div class="alert alert-success" role="alert"><strong>Well done!</strong> You successfully commented on ' + title + '. ' + link + '</div>';
+// 	// panel.replaceWithPush(contents).delay(1000).fadeOut();
+// });
