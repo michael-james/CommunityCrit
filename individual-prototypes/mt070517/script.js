ff5115d74424f4cbcefd93eaffad010ae89b02ca
Michael James <thelivingpc@gmail.com>
2017-07-05 21:09:31 -0700

    finished dev prototype list and detail

--- individual-prototypes/mt070517/script.js
+++ individual-prototypes/mt070517/script.js
@@ -1,37 +1,44 @@
 $( document ).ready(function() {
-  $("#shareBtns button").click( function() {
-  	var shareType = $(this).val();
-  	createShareForm(shareType);
-  });
+  	$("#shareBtns button").click( function() {
+  		var shareType = $(this).val();
+  		createShareForm(shareType);
+  	});
 });
+
+
+      //     
+      //       Timothy, 03/23/17
+      //     </div> <!-- .panel-footer -->
+      //   </div> <!-- .panel -->
+      // </div> <!-- .col -->
 
 function createShareForm(shareType) {
 	var data;
 
 	switch(shareType) {
     case "pro":
-        data = ["I <strong>like</strong>...", shareType + "1", , "this element of the project",
-        		shareType + "2", "because", "of this reason."];
+        data = ["I <strong>like</strong>...", ,"this element of the project",
+        		"because", "of this reason."];
         break;
     case "con":
-        data = ["I am <strong>concerned</strong> about...", shareType + "1", , "this element of the project", 
-        		shareType + "2", "because", "of this reason.",
-        		shareType + "3", "One way to address this would be", "to follow this suggestion."];
+        data = ["I am <strong>concerned</strong> about...", , "this element of the project", 
+        		"because", "of this reason.",
+        		"One way to address this would be", "to follow this suggestion."];
         break;
     case "suggestion":
-        data = ["I have a <strong>suggestion</strong>...", shareType + "1", "What if", "the project included this."];
+        data = ["I have a <strong>suggestion</strong>...", "What if", "the project included this."];
         break;
     case "question":
-        data = ["I have a <strong>question</strong> about...", shareType + "1", , "this element of the project", 
-        		shareType + "2", "I would like more information about", "this aspect of that element."];
+        data = ["I have a <strong>question</strong> about...", , "this element of the project", 
+        		"I would like more information about", "this aspect of that element."];
         break;
     case "story":
-        data = ["I have a <strong>story</strong> about...", shareType + "1", , "this element of the project", 
-        		shareType + "2", "Once upon a time,", "this thing happened to me or someone I know."];
+        data = ["I have a <strong>story</strong> about...", , "this element of the project", 
+        		"Once upon a time,", "this thing happened to me or someone I know."];
         break;
     case "reference":
-        data = ["This makes me <strong>think of</strong>...", shareType + "1", , "this thing I know or this link.", 
-        		shareType + "2", "We could learn", "this from that."];
+        data = ["This makes me <strong>think of</strong>...", , "this thing I know or this link.", 
+        		"We could learn", "this from that."];
         break;
     default:
         data = [];
@@ -40,17 +47,14 @@ function createShareForm(shareType) {
 	console.log(data.length);
 
 	var title = data[0];
-	var input1id = data[1];
-	var input1label = data[2];
-	var input1placeholder = data[3];
-	var input2id = data[4]; 
-	var input2label = data[5];
-	var input2placeholder = data[6];
-	var input3id = data[7];
-	var input3label = data[8];
-	var input3placeholder = data[9];
+	var input1label = data[1];
+	var input1placeholder = data[2];
+	var input2label = data[3];
+	var input2placeholder = data[4];
+	var input3label = data[5];
+	var input3placeholder = data[6];
 
-	var html =	'<div class="col-sm-6 col-md-4">' +
+	var html =	'<div class="col-sm-6">' +
 					'<div class="panel panel-primary">' +
 						'<div class="panel-heading">' +
 							'<div class="panel-title">' + title + '</div>' +
@@ -60,39 +64,56 @@ function createShareForm(shareType) {
 								'<div class="form-group">';
 
 	if (input1label != undefined) {
-		html += '<label for="' + input1id + '">' + input1label +'</label>';
+		html += '<label for="input1">' + input1label +'</label>';
 	}
 									
-	html += '<textarea class="form-control" id="' + input1id + '" placeholder="' + input1placeholder +' "></textarea>' +
+	html += '<textarea class="form-control" id="input1" placeholder="' + input1placeholder +' "></textarea>' +
 		'</div>';
 
 
-	if (data.length > 4) {
+	if (data.length > 3) {
 		console.log("second input added");
 		html += '<div class="form-group">' +
-					'<label for="' + input2id + '">' + input2label +'</label>' +
-					'<textarea class="form-control" id="' + input2id + '" placeholder="' + input2placeholder + '"></textarea>' +
+					'<label for="input2">' + input2label +'</label>' +
+					'<textarea class="form-control" id="input2" placeholder="' + input2placeholder + '"></textarea>' +
 				'</div>';
 	}
 
-	if (data.length == 8) {
+	if (data.length == 6) {
 		console.log("third label added");
 		html +=	'<div class="form-group">' +
-					'<label for="' + input3id + '">' + input3label + '</label>' +
+					'<label for="input3">' + input3label + '</label>' +
 				'</div>';
 	}
 	
-	if (data.length > 7) {
+	if (data.length > 5) {
 		console.log("third input added");
 		html += '<div class="form-group">' +
-					'<label for="' + input3id + '">' + input3label + '</label>' +
-					'<textarea class="form-control" id="' + input3id + '" placeholder="' + input3placeholder + '"></textarea>' +
+					'<label for="input3">' + input3label + '</label>' +
+					'<textarea class="form-control" id="input3" placeholder="' + input3placeholder + '"></textarea>' +
 				'</div>';
 	}
 		
-	html += 	'<input type="submit" value="Submit" class="btn btn-default">' +
+	html += 	'<input type="button" value="Submit" class="btn btn-default" onclick="return false;">' +
 			'</form>' +
-		'</div> <!-- .panel-body --></div> <!-- .panel --></div> <!-- .col-sm-6 col-md-4 -->';
+		'</div> <!-- .panel-body --></div> <!-- .panel --></div> <!-- .col -->';
 
-	$("#shareForms").append(html);
+	$("#shareForms").prepend(html);
 }
+
+// $("input:share").click(function() {
+// 	var panel = $(e).parentsUntil("#shareForms");
+
+// 	var input1val, input2val, input3val = "hi";
+	
+// 	var html = 	'<div class="col-md-6"><div class="panel panel-default">' + 
+// 	        		'<div class="panel-body">' +
+// 	        			'<p>' + input1val + ' ' + input2val + ' ' + input3val + '</p>' +
+// 	        		'</div> <!-- .panel-body -->' +
+// 	        		'<div class="panel-footer">' +
+// 	        			'FirstName' + ', ' + '00/00/00' +
+// 	        		'</div> <!-- .panel-footer --></div> <!-- .panel --></div> <!-- .col -->';
+
+// 	panel.replaceWith(contents);
+// });
+
